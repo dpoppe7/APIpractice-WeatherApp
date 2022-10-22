0e58d908d22bc22165680c00bd4036d70f3e0f9f
Damaris <dpoppe@harding.edu>
2022-10-22 12:41:03 -0500

    Initial Commit

---
+++ Shared/APIpracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  APIpracticeApp.swift
+//  Shared
+//
+//  Created by Damaris Poppe on 10/22/22.
+//
+
+import SwiftUI
+
+@main
+struct APIpracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
